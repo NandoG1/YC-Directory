0db0fb2951379eac896fcba5ecbf0db2b5a6ff19
NandoG1 <fernandogunawan291105@gmail.com>
2025-06-04 11:34:26 +0700

    adding auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-red-500 text-3xl">Hello World!</h1>
-    </div>
+    <>
+      <h1 className="uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] text-center my-5">Home</h1>
+    </>
   );
 }
